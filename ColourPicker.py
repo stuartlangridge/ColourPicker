a07a6ca9e922cb81d0f41f9d9cf0b8385471d977
Stuart Langridge <sil@kryogenix.org>
2016-05-13 23:14:11 +0100

    Rewrite to not use a TreeView, because CellRenderers are rubbish and customising them is really hard. We're keeping the old code around for a bit in case we need it for something, but it's not used and will be gone soon.

--- ColourPicker.py
+++ ColourPicker.py
@@ -673,6 +673,277 @@ LAB_COLOUR_NAMES = [(rgb_to_lab(x[0],x[1],x[2]), x[3]) for x in COLOUR_NAMES]
 
 class Main(object):
     def __init__(self):
+        # useful globals
+        self.snapsize = (80, 80)
+        self.closest_name_cache = {}
+        self.history = []
+        self.colour_text_labels = []
+        self.grabbed = False
+
+        # the window
+        self.w = Gtk.Window()
+        self.w.set_title("Colour Picker")
+        self.w.set_size_request(460, 500)
+        self.w.connect("motion-notify-event", self.magnifier_move)
+        self.w.connect("button-press-event", self.magnifier_clicked)
+        self.w.connect("destroy", Gtk.main_quit)
+        self.pointer = self.w.get_screen().get_display().get_device_manager().get_client_pointer()
+
+        # the headerbar
+        head = Gtk.HeaderBar()
+        head.set_show_close_button(True)
+        head.props.title = "Colour Picker"
+        self.w.set_titlebar(head)
+        btngrab = Gtk.Button()
+        icon = Gio.ThemedIcon(name="find-location-symbolic")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        btngrab.add(image)
+        head.pack_start(btngrab)
+        btngrab.connect("clicked", self.grab)
+
+        # the box that history items go in
+        self.vb = Gtk.VBox()
+
+        # the status bar
+        hb = Gtk.HBox()
+        self.formatters = {
+            "CSS hex": lambda r, g, b: "#%02x%02x%02x" % (r, g, b),
+            "CSS uppercase hex": lambda r, g, b: ("#%02x%02x%02x" % (r, g, b)).upper(),
+            "CSS rgb": lambda r, g, b: "rgb(%s, %s, %s)" % (r, g, b),
+            "CSS rgba": lambda r, g, b: "rgba(%s, %s, %s, 1)" % (r, g, b)
+        }
+        formats = Gtk.ListStore(str)
+        for fr, fn in self.formatters.items():
+            formats.append((fr,))
+        fcom = Gtk.ComboBox.new_with_model(formats)
+        fcell = Gtk.CellRendererText()
+        fcom.pack_start(fcell, expand=True)
+        fcom.add_attribute(fcell, "text", 0)
+        vcell = Gtk.CellRendererText()
+        fcom.pack_start(vcell, True)
+        fcom.set_cell_data_func(vcell, self.formatRGB)
+        vcell.set_property("weight", 200)
+        vcell.set_property('xalign', 1.0)
+        fcom.set_active(0)
+        self.active_formatter = "CSS hex"
+        fcom.connect("changed", self.change_format)
+        hb.pack_end(fcom, False, False, 0)
+        self.vb.pack_end(hb, False, False, 0)
+
+        # and, go
+        self.w.add(self.vb)
+        self.w.show_all()
+        GLib.idle_add(self.load_history)
+
+    def grab(self, btn):
+        print "grab"
+        self.grabbed = True
+        self.w.set_opacity(0)
+        GLib.timeout_add(5000, self.ungrab)
+        self.set_magnifier_cursor()
+
+    def set_magnifier_cursor(self):
+        root = Gdk.get_default_root_window()
+        pointer, px, py = self.pointer.get_position()
+        self.latest_pb = self.snap(
+            px-(self.snapsize[0]/2), py-(self.snapsize[1]/2), 
+            self.snapsize[0], self.snapsize[1])
+        scaled_pb = self.latest_pb.scale_simple(
+            self.snapsize[0] * 2, self.snapsize[1] * 2, GdkPixbuf.InterpType.TILES)
+        scaled_pb_subset = scaled_pb.new_subpixbuf(
+            self.snapsize[0] / 2, self.snapsize[1] / 2, self.snapsize[0], self.snapsize[1])
+
+        surface = Gdk.cairo_surface_create_from_pixbuf(scaled_pb_subset, 0, None)
+        context = cairo.Context(surface)
+        context.set_source_rgba(1, 0.5, 0.5, 1)
+        context.set_line_width(4)
+        context.arc(self.snapsize[0] / 2, self.snapsize[1] / 2, self.snapsize[0] / 2, 0, 2*math.pi)
+        context.stroke()
+        drawn_pb = Gdk.pixbuf_get_from_surface(surface, 0, 0, surface.get_width(), surface.get_height())
+
+        magnifier = Gdk.Cursor.new_from_pixbuf(
+            self.w.get_screen().get_display(), 
+            drawn_pb,
+            self.snapsize[0]/2, self.snapsize[1]/2)
+
+        self.pointer.grab(
+            self.w.get_window(),
+            Gdk.GrabOwnership.APPLICATION,
+            True,
+            Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.POINTER_MOTION_MASK | Gdk.EventMask.SCROLL_MASK,
+            magnifier,
+            Gdk.CURRENT_TIME)
+
+    def ungrab(self, *args, **kwargs):
+        print "ungrabbing", args, kwargs
+        self.pointer.ungrab(Gdk.CURRENT_TIME)
+        self.grabbed = False
+        self.w.set_opacity(1)
+
+    def get_cache_file(self):
+        return os.path.join(GLib.get_user_cache_dir(), "colour-picker.json")
+
+    def serialise(self, *args, **kwargs):
+        # yeah, yeah, supposed to use Gio's async file stuff here. But it was writing
+        # corrupted files, and I have no idea why; probably the Python var containing
+        # the data was going out of scope or something. Anyway, we're only storing
+        # five small images, so life's too short to hammer on this; we'll write with
+        # Python and take the hit.
+        fp = codecs.open(self.get_cache_file(), encoding="utf8", mode="w")
+        json.dump({"colours": self.history}, fp, indent=2)
+        fp.close()
+
+
+    def add_history_item(self, r, g, b, base64_imgdata=None, pixbuf=None):
+        def show_copy(eb, ev, img): img.set_opacity(1)
+        def hide_copy(eb, ev, img): img.set_opacity(0)
+        def clipboard(*args):
+            colour = self.formatters[self.active_formatter](r, g, b)
+            Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD).set_text(colour, len(colour))
+
+        eb = Gtk.EventBox()
+        hb = Gtk.HBox()
+        eb.add(hb)
+
+        if base64_imgdata:
+            loader = GdkPixbuf.PixbufLoader.new_with_type("png")
+            loader.write(base64_imgdata.decode("base64"))
+            pb = loader.get_pixbuf()
+            loader.close()
+            i = Gtk.Image.new_from_pixbuf(pb)
+        elif pixbuf:
+            i = Gtk.Image.new_from_pixbuf(pixbuf)
+            success, data = pixbuf.save_to_bufferv("png", [], [])
+            base64_imgdata = data.encode("base64")
+        else:
+            raise Exception("A history item must have either imgdata or a pixbuf")
+        hb.pack_start(i, False, False, 6)
+
+        str_colour = self.formatters["CSS hex"](r, g, b)
+        gdk_colour = Gdk.color_parse(str_colour)
+        gdk_rgba = Gdk.RGBA.from_color(gdk_colour)
+        area = Gtk.DrawingArea()
+        area.set_size_request(self.snapsize[0]/2, self.snapsize[1]/2)
+        area.override_background_color(0, gdk_rgba)
+        hb.pack_start(area, False, False, 6)
+
+        lbl = Gtk.Label()
+        self.colour_text_labels.append(lbl)
+        self.set_colour_label_text(lbl, r, g, b)
+        lbl.set_halign(Gtk.Align.START)
+        hb.pack_start(lbl, True, True, 6)
+
+        copy = Gtk.Image.new_from_icon_name("edit-copy-symbolic", 0)
+        copy.set_opacity(0)
+        hb.pack_start(copy, False, False, 6)
+
+        eb.connect("enter-notify-event", show_copy, copy)
+        eb.connect("leave-notify-event", hide_copy, copy)
+        eb.connect("button-press-event", clipboard, r, g, b)
+        eb.set_tooltip_text("Copy to clipboard")
+
+        self.vb.pack_start(eb, False, False, 12)
+        self.vb.reorder_child(eb, 0)
+        self.vb.show_all()
+        eb.show_all()
+
+        self.history.append({
+            "imgdata": base64_imgdata,
+            "colour": [r, g, b]
+        })
+
+        if len(self.history) > 5:
+            del self.history[5]
+            self.vb.get_children()[4].destroy()
+
+    def set_colour_label_text(self, lbl, r, g, b):
+        lbl.set_markup("%s\n%s" % (
+            self.closest_name(r, g, b), self.formatters[self.active_formatter](r, g, b)))
+
+    def finish_loading_history(self, f, res):
+        try:
+            success, contents, _ = f.load_contents_finish(res)
+            data = json.loads(contents)
+            colours = data["colours"]
+            for item in colours:
+                self.add_history_item(
+                    item["colour"][0], item["colour"][1], item["colour"][2],
+                    base64_imgdata=item["imgdata"]
+                )
+        except:
+            print "Failed to restore data"
+            raise
+
+    def load_history(self):
+        f = Gio.File.new_for_path(self.get_cache_file())
+        f.load_contents_async(None, self.finish_loading_history)
+
+    def magnifier_clicked(self, *args, **kwargs):
+        if self.grabbed:
+            self.ungrab()
+            colour = self.get_colour_from_pb(self.latest_pb)
+            pbcopy = self.latest_pb.scale_simple(self.snapsize[0] / 2,
+                self.snapsize[1] / 2, GdkPixbuf.InterpType.TILES)
+            self.add_history_item(colour[0], colour[1], colour[2], pixbuf=pbcopy)
+            GLib.idle_add(self.serialise)
+
+    def get_colour_from_pb(self, pb):
+        pixel_data = pb.get_pixels()
+        offset = (pb.get_rowstride() * (self.snapsize[1] / 2)) + ((self.latest_pb.get_rowstride() / self.snapsize[0]) * (self.snapsize[0] / 2))
+        rgb_vals = tuple([ord(x) for x in pixel_data[offset:offset+3]])
+        return rgb_vals
+
+    def magnifier_move(self, *args, **kwargs):
+        if not self.grabbed: return
+        self.set_magnifier_cursor()
+
+    def change_format(self, cb):
+        self.active_formatter = cb.get_model().get_value(cb.get_active_iter(), 0)
+        for lbl, hist in zip(self.colour_text_labels, self.history):
+            self.set_colour_label_text(lbl, hist["colour"][0], hist["colour"][1], hist["colour"][2])
+
+    def closest_name(self, r, g, b):
+        max_deltaE_found = 999999999
+        col = self.closest_name_cache.get((r, g, b))
+        if col is not None: return col
+        labcol = rgb_to_lab(r, g, b)
+        for reflabcol, name in LAB_COLOUR_NAMES:
+            dE = deltaE(labcol, reflabcol)
+            if dE < max_deltaE_found:
+                col = name
+                max_deltaE_found = dE
+        self.closest_name_cache[(r, g, b)] = col
+        return col
+
+    def formatRGB(self, column, cell_renderer, model, iter):
+        formatter = self.formatters.get(model.get_value(iter, 0))
+        text = "?"
+        if formatter:
+            text = formatter(255, 255, 255)
+        cell_renderer.set_property("text", text)
+
+    def snap(self, x, y, w, h):
+        display=Gdk.Display.get_default()
+        (screen,self.x,self.y,modifier) = display.get_pointer()
+        root = Gdk.get_default_root_window()
+        screenshot = Gdk.pixbuf_get_from_window(root, x, y, w, h)
+        return screenshot
+
+
+class foo:
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------
+
+    def x__init__(self):
         self.snapsize = (160, 160)
         self.closest_name_cache = {}
 
@@ -824,60 +1095,30 @@ class Main(object):
             text = formatter(255, 255, 255)
         cell_renderer.set_property("text", text)
 
+    def get_colour_from_pb(self, pb):
+        pixel_data = self.latest_pb.get_pixels()
+        offset = (self.latest_pb.get_rowstride() * (self.snapsize[1] / 2)) + ((self.latest_pb.get_rowstride() / self.snapsize[0]) * (self.snapsize[0] / 2))
+        rgb_vals = tuple([ord(x) for x in pixel_data[offset:offset+3]])
+        return rgb_vals
+
     def window_clicked(self, *args, **kwargs):
         if self.grabbed:
             self.ungrab()
+            colour = self.get_colour_from_pb(self.latest_pb)
+            pbcopy = self.latest_pb.scale_simple(self.snapsize[0] / 2, self.snapsize[1] / 2, GdkPixbuf.InterpType.TILES)
+            self.remembered.prepend((pbcopy, colour[0], colour[1], colour[2], False, "Copy to clipboard"))
+            itr = self.remembered.iter_nth_child(None, 5)
+            while itr:
+                self.remembered.remove(itr)
+                itr = self.remembered.iter_nth_child(None, 5)
+
+            GLib.idle_add(self.serialise)
 
     def window_move(self, *args, **kwargs):
         # FIXME the problem is here. How do we update the cursor? Editing the pixbuf 
         # we created it with doesn't work. Setting the cursor on our window doesn't work
         if not self.grabbed: return
         self.set_magnifier_cursor()
-
-    def grab(self, btn):
-        print "grab"
-        self.grabbed = True
-        self.w.set_opacity(0)
-        GLib.timeout_add(5000, self.ungrab)
-        self.set_magnifier_cursor()
-
-    def set_magnifier_cursor(self):
-        root = Gdk.get_default_root_window()
-        pointer, px, py = self.pointer.get_position()
-        screen_pb = self.snap(
-            px-(self.snapsize[0]/2), py-(self.snapsize[1]/2), 
-            self.snapsize[0], self.snapsize[1])
-        scaled_pb = screen_pb.scale_simple(
-            self.snapsize[0] * 2, self.snapsize[1] * 2, GdkPixbuf.InterpType.TILES)
-        scaled_pb_subset = scaled_pb.new_subpixbuf(
-            self.snapsize[0] / 4, self.snapsize[1] / 4, self.snapsize[0] / 2, self.snapsize[1] / 2)
-
-        surface = Gdk.cairo_surface_create_from_pixbuf(scaled_pb_subset, 0, None)
-        context = cairo.Context(surface)
-        context.set_source_rgba(1, 0.5, 0.5, 1)
-        context.set_line_width(4)
-        context.arc(self.snapsize[0] / 2, self.snapsize[1] / 2, self.snapsize[0] / 2, 0, 2*math.pi)
-        context.stroke()
-        drawn_pb = Gdk.pixbuf_get_from_surface(surface, 0, 0, surface.get_width(), surface.get_height())
-
-        magnifier = Gdk.Cursor.new_from_pixbuf(
-            self.w.get_screen().get_display(), 
-            drawn_pb,
-            self.snapsize[0]/2, self.snapsize[1]/2)
-
-        self.pointer.grab(
-            self.w.get_window(),
-            Gdk.GrabOwnership.APPLICATION,
-            True,
-            Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.POINTER_MOTION_MASK | Gdk.EventMask.SCROLL_MASK,
-            magnifier,
-            Gdk.CURRENT_TIME)
-
-    def ungrab(self, *args, **kwargs):
-        print "ungrabbing", args, kwargs
-        self.pointer.ungrab(Gdk.CURRENT_TIME)
-        self.grabbed = False
-        self.w.set_opacity(1)
 
     def row_clicked(self, sel, store, path, selected, data):
         r = store.get_value(store.get_iter(path), 1)
@@ -937,36 +1178,6 @@ class Main(object):
 
         return True
 
-    def keypress(self, *args, **kwargs):
-        pbcopy = self.latest_pb.scale_simple(self.snapsize[0], self.snapsize[1], GdkPixbuf.InterpType.TILES)
-        self.remembered.prepend((pbcopy, self.colour.get_text()))
-        itr = self.remembered.iter_nth_child(None, 5)
-        while itr:
-            self.remembered.remove(itr)
-            itr = self.remembered.iter_nth_child(None, 5)
-
-        GLib.idle_add(self.serialise)
-
-    def get_cache_file(self):
-        return os.path.join(GLib.get_user_cache_dir(), "colour-picker.json")
-
-    def serialise(self, *args, **kwargs):
-        #print "serialise", self.remembered
-        lst = []
-        for pb, col in self.remembered:
-            success, data = pb.save_to_bufferv("png", [], [])
-            lst.append({
-                "imgdata": data.encode("base64"),
-                "colour": col
-            })
-        # yeah, yeah, supposed to use Gio's async file stuff here. But it was writing
-        # corrupted files, and I have no idea why; probably the Python var containing
-        # the data was going out of scope or something. Anyway, we're only storing
-        # five small images, so life's too short to hammer on this; we'll write with
-        # Python and take the hit.
-        fp = codecs.open(self.get_cache_file(), encoding="utf8", mode="w")
-        json.dump({"colours": lst}, fp, indent=2)
-        fp.close()
 
 if __name__ == "__main__":
     Main()
