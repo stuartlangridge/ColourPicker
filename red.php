cb44e353e66a413ccced2fb27b82aa387626f3c4
Stuart Langridge <sil@kryogenix.org>
2016-05-17 16:20:17 +0100

    Actually redirect to the deb

--- red.php
+++ red.php
@@ -3,7 +3,7 @@ $f = file_get_contents("https://code.launchpad.net/~sil/+archive/ubuntu/pick/+pa
 preg_match('/<a href="([^"]+)_source.changes"/', $f, $m);
 if (count($m) == 2) {
     $nurl = $m[1] . "_all.deb";
-    echo $nurl;
+    header("Location: $nurl");
 } else {
     echo 'Download does not seem to be available. Perhaps you can get Pick from <a href="https://code.launchpad.net/~sil/+archive/ubuntu/pick/">the Pick PPA</a>.';
 }
